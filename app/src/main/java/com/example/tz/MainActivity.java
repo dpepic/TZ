9c71712dc1538d2996f69d9fdfb9cf2215150003
JuzerNejm <deltolakne@ezehe.com>
2019-06-13 11:36:24 +0200

    Circular??

--- app/src/main/java/com/example/tz/MainActivity.java
+++ app/src/main/java/com/example/tz/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+
 }
